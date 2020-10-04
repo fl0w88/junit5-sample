9430c2e0aa98d4c2b5aa9bf63c14f77f99e14fb1
Florin Balint <florin.balint@oebb.at>
2020-10-04 20:34:16 +0200

    init and commit

---
+++ src/test/java/com/example/project/FirstTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+@Tag("fast")
+class FirstTest {
+
+	@Test
+	@DisplayName("My 1st JUnit 5 test! 😎")
+	void myFirstTest(TestInfo testInfo) {
+
+	}
+
+}
---
+++ src/test/java/com/example/project/WordSorterTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class WordSorterTests {
+
+    private List<String> wordsToSort;
+
+    @BeforeEach
+    public void initList() {
+        wordsToSort = Arrays.asList("", "", "", "", "");
+    }
+
+    @Test
+    void testOrder() {
+	}
+
+}
