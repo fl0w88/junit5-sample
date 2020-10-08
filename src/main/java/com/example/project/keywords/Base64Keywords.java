56e3ba52871f3f36634f9c9a2a10e5e702233077
Florin Balint <florin.balint@oebb.at>
2020-10-08 12:19:32 +0200

    updating tests

--- src/main/java/com/example/project/keywords/Base64Keywords.java
+++ src/main/java/com/example/project/keywords/Base64Keywords.java
@@ -1,16 +1,23 @@
 package com.example.project.keywords;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 public class Base64Keywords {
 
     private static WebDriver driver;
-    private WebDriverWait wait = null;
+    private static WebDriverWait wait = null;
 
     private void init() {
         WebDriverManager.chromedriver().setup();
@@ -31,4 +38,38 @@ public class Base64Keywords {
         driver.get("https://www.base64encode.org/");
     }
 
+    public void closeCookies() {
+        By inputFrom = By.xpath("//button[text() = 'AGREE']");
+        wait.until(elementToBeClickable(inputFrom));
+        WebElement element = driver.findElement(inputFrom);
+        element.sendKeys(Keys.ENTER);
+
+        waitALittle();
+    }
+
+    public String encodeTest() {
+        By inputFrom = By.id("input");
+        wait.until(elementToBeClickable(inputFrom));
+        WebElement element = driver.findElement(inputFrom);
+        element.sendKeys("muster");
+
+        By buttonSubmitBy = By.id("submit_text");
+        wait.until(elementToBeClickable(buttonSubmitBy));
+        element = driver.findElement(buttonSubmitBy);
+        element.sendKeys(Keys.ENTER);
+        waitALittle();
+
+        By output = By.id("output");
+        wait.until(elementToBeClickable(output));
+        element = driver.findElement(output);
+        return element.getText();
+    }
+
+    public static void waitALittle() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/example/project/taf/tests/Base64Test.java
+++ src/test/java/com/example/project/taf/tests/Base64Test.java
@@ -1,5 +1,7 @@
 package com.example.project.taf.tests;
 
+import com.example.project.keywords.Base64Keywords;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Base64Test extends TestCaseBase {
@@ -7,6 +9,14 @@ public class Base64Test extends TestCaseBase {
     @Test
     public void testBase64Conversion() {
         b64App().base64Keywords().openBase64Site();
+
+        b64App().base64Keywords().closeCookies();
+
+        String testResult = b64App().base64Keywords().encodeTest();
+
+        Assertions.assertEquals("bXVzdGVy", testResult);
+
+        Base64Keywords.waitALittle();
     }
 
 }
